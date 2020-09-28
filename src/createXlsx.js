b0b11ca73b76dab2dade9ada18c035cba5892f30
loclv <masamonedante@gmail.com>
2020-09-28 09:16:11 +0700

    init: create xlsx from json

--- src/createXlsx.js
+++ src/createXlsx.js
@@ -5,8 +5,15 @@ function createXlsx(name) {
 
   // Load a new blank workbook
   XlsxPopulate.fromBlankAsync().then((workbook) => {
-    // Modify the workbook.
-    workbook.sheet('Sheet1').cell('A1').value('This is neat!');
+    const sheet1 = workbook.sheet('Sheet1');
+
+    sheet1.column('B').width(24);
+
+    for (let i = 1; i <= 10; i++) {
+      // Modify the workbook.
+      sheet1.cell(`A${i}`).value('This is key!');
+      sheet1.cell(`B${i}`).value('This is value!');
+    }
 
     // Write to file.
     return workbook.toFileAsync(`./${name}.xlsx`);
