94a999d7c0cc2192ab52cecbb6e8d5a1dd001771
loclv <masamonedante@gmail.com>
2020-09-28 09:46:59 +0700

    init: read xlsx

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const XLSX = require('xlsx-populate');
 const dotenv = require('dotenv');
 
 const createXlsx = require('./src/createXlsx');
@@ -7,6 +6,10 @@ const readXlsx = require('./src/readXlsx');
 dotenv.config();
 
 const inputName = process.env.INPUT_NAME;
+
+const readObj = readXlsx(inputName);
+console.log(readObj);
+
 const outputName = process.env.OUTPUT_NAME;
 
 const object = {a: 1, b: 2, c: 3};
